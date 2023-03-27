6ff6c2007cbf4fe31305307fc8bddb18952702c8
Kevin Salinas <kevinsalinas155@gmail.com>
2023-03-27 17:20:58 -0400

    added backend setup

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,1 +1,9 @@
-# testing testing 
+#!/usr/bin/env python3
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+# this is what we edited in line 58, you can name it w.e
+engine = create_engine('sqlite:///migrations_dance_match_app.db')
+
+Base = declarative_base()
+
