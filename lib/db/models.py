4d838253f0d4150bfab84b67695067006094e6ce
Kevin Salinas <kevinsalinas155@gmail.com>
2023-03-27 17:32:39 -0400

    added columns to classes

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+
+from sqlalchemy import Column, Integer, String
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -7,3 +10,21 @@ engine = create_engine('sqlite:///migrations_dance_match_app.db')
 
 Base = declarative_base()
 
+class Instructor(Base):
+    __tablename__ = 'instructors'
+
+    id = Column(Integer(), primary_key = True)
+    name = Column(String(), index=True)
+
+class Lessons(Base):
+    __tablename__ = 'instructors'
+
+    id = Column(Integer(), primary_key = True)
+    type = Column(String(), index=True)
+
+
+class Dancers(Base):
+    __tablename__ = 'dancers'
+
+    id = Column(Integer(), primary_key = True)
+    name = Column(String(), index=True)
