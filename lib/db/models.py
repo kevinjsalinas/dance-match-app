048fde66da6a7416ab31b64b81b47cfb245f7644
Beau Kim <beaukim3@gmail.com>
2023-03-28 18:35:00 -0400

    fixed table data

--- lib/db/models.py
+++ lib/db/models.py
@@ -4,7 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String, ForeignKey
 
 # from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref, declarative_base
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
 
 import ipdb
 
